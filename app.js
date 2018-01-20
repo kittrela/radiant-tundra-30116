98bf680123a00e90767bf538885bd3c55dad952c
Vadiwoo <karuppiv@oregonstate.edu>
2018-01-19 20:23:46 -0800

    changes made

--- app.js
+++ app.js
@@ -2,37 +2,33 @@ var express = require('express');
 const path = require('path');
 
 var app = express();
-var handlebars = require('express-handlebars').create({ defaultLayout: 'main', partialsDir: ['views/partials/'] });
-
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+var port = app.listen(process.env.PORT || 3000);
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 //app.set('port', 3000);
-var port = process.env.PORT || 3000;
 
-//handlebars.registerPartials(__dirname + '/views/partials');
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function (req, res) {
-    res.render('home') //We can omit the .handlebars extension as we do below
+app.get('/',function(req,res){
+  res.render('home') //We can omit the .handlebars extension as we do below
 });
 
-app.get('/other-page', function (req, res) {
-    res.render('other-page');
+app.get('/other-page',function(req,res){
+  res.render('other-page');
 });
 
-app.use(function (req, res) {
-    res.status(404);
-    res.render('404');
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
 });
 
-app.use(function (err, req, res, next) {
-    console.error(err.stack);
-    res.type('plain/text');
-    res.status(500);
-    res.render('500');
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.render('500');
 });
 
 app.listen(port);
-
-
 
