61ebf069824a083cdf650eb4e19353cfe8acec34
Vadiwoo <karuppiv@oregonstate.edu>
2018-01-29 15:51:40 -0800

    authentication isssues

--- routes/sign-in.js
+++ routes/sign-in.js
@@ -5,15 +5,15 @@ const passport = require('passport');
 const { Client } = require('pg');
 
 const client = new Client({
-  connectionString : "pg://nhwljdkwkfhnoy:7a2f32711c734a5d67cfc0a1e59acc91654193795d8655de7ae0cfb27b630390@ec2-174-129-22-84.compute-1.amazonaws.com:5432/dfh46ttrtrflgb",
-  ssl: true,
+    connectionString : "pg://nhwljdkwkfhnoy:7a2f32711c734a5d67cfc0a1e59acc91654193795d8655de7ae0cfb27b630390@ec2-174-129-22-84.compute-1.amazonaws.com:5432/dfh46ttrtrflgb",
+    ssl: true,
 });
 client.connect((err) => {
-  if (err) {
-    console.error('connection error', err.stack)
-  } else {
-    console.log('connected')
-  }
+    if (err) {
+        console.error('connection error', err.stack)
+    } else {
+        console.log('connected')
+    }
 })
 
 router.get('/', (req, res) => {
@@ -21,41 +21,52 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', function(req, res,next) {
-	console.log(req.body);
-	console.log("hello");
-	client.query('SELECT * FROM employee;', (err, res,body) => {
-  if (err) throw err;
-  //for (let row of res.rows) {
-    //console.log(JSON.stringify(row));
-    var context = {};
-    var results=[]; 
-    var jsonResult = JSON.parse(body);
-    for(var entry in jsonResult.Data){
-			results.push({
+    //console.log(req.body);
+    var username = req.body.username;
+    var password = req.body.password;
+    console.log(username);
+    console.log(password);
+    //console.log("hello");
+	
+    client.query('SELECT employee_email,employee_password, employee_type  FROM employee', (err, res,body) => {
+        if (err) throw err;
+        var context = {};
+        var results=[];
+        for (let row of res.rows) {
+            var jsonResult = (JSON.stringify(row));
+  	
+   
+            var context = {};
+            var results=[];
+            for (var i of jsonResult)
+            {
+                results.push({
 				
-				employee_id: jsonResult.Data[entry].id,
-				employee_email: jsonResult.Data[entry].email,
-				employee_password: jsonResult.Data[entry].password,
-				access_date: jsonResult.Data[entry].date,
-				signature: jsonResult.Data[entry].signature,
-				employee_type: jsonResult.Data[entry].type,
-				admin_id: jsonResult.Data[entry].admin_id,
-				employee_first_name: jsonResult.Data[entry].first_name,	
-				employee_last_name	: jsonResult.Data[entry].last_name	
+                    employee_email: jsonResult.Data[i].employee_email,
+                    employee_password: jsonResult.Data[i].employee_password,
+                    employee_type: jsonResult.Data[i].employee_type,
+			
 						
-				}
-			);
-		if(results.email === username && results.password === password){
-    		console.log("success")
-    		res.render('/dashboard');
-   		 }
-		}
-    
+                });
+        //console.log(jsonResult);
+            };
+   	
+            console.log("User Email: " + results.Data[0].employee_email); 
+ 
+
+    	
+    	
+    		
+        };
+    });
+});
+   	
+		
+    	
   
- // client.end();
-});
-    //res.render('/dashboard');
-});
+// client.end();
+
+//res.render('/dashboard');
 
 /*
 router.post('/', passport.authenticate('local'), (req, res) => {
